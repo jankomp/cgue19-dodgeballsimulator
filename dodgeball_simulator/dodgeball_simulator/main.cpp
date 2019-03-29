9931bcea3191bedb28f0dea8b9e6b32488c28aa9
jan <jankompatscher@gmail.com>
2019-03-29 14:29:40 +0100

    implemented camera.h Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	dodgeball_simulator/.vs/dodgeball_simulator/v15/.suo
    #	dodgeball_simulator/.vs/dodgeball_simulator/v15/Browse.VC.db

--- dodgeball_simulator/dodgeball_simulator/main.cpp
+++ dodgeball_simulator/dodgeball_simulator/main.cpp
@@ -1,12 +1,66 @@
-#include "main.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <GL/GL.h>
+
+#include "shader.h"
+#include "geometry.h"
+#include "camera.h"
 
 
-
-main::main()
+int main(void)
 {
-}
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "dodgeball simulator", NULL, NULL);
+	
+	if (!window)
+	{
+		glfwTerminate();
+		std::printf("Failed to create window");
+		//exit;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = true;
+	GLenum err = glewInit();
+
+	// If GLEW wasn't initialized
+	if (err != GLEW_OK) {
+		std::printf("Failed to init GLEW");
+		//exit;
+	}
+
+	shader testShader;
+	testShader.loadShader("shaders/basic.vert", "shaders/basic.frag");
+
+	//geometry cube = geometry(glm::translate(glm::mat4(1.0f), glm::vec3(-1.2f, -1.5f, 0.0f)), geometry::createCubeGeometry(1.5f, 1.5f, 1.5f));
+	//geometry sphere = geometry(glm::translate(glm::mat4(1.0f), glm::vec3(-1.2f, 1.0f, 0.0f)), geometry::createSphereGeometry(32, 16, 1.0f));
 
 
-main::~main()
-{
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+
+		/*draw sphere*/
+		//sphere.draw();
+	}
+
+	glfwTerminate();
+	return 0;
 }
