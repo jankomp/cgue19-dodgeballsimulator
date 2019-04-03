f69413fd25e7fdf5ead18b0997475a2819e1252b
jan <jankompatscher@gmail.com>
2019-04-03 18:52:46 +0200

    Camera: rotate() u. updatePlayerPosition() implementiert

--- dodgeball_simulator/dodgeball_simulator/Camera.cpp
+++ dodgeball_simulator/dodgeball_simulator/Camera.cpp
@@ -2,12 +2,26 @@
 #include <glm/gtx/transform.hpp>
 
 
-Camera::Camera() : viewDirection(0.0f, 0.0f, -1.0f), UP(0.0f, 1.0f, 0.0f)
+Camera::Camera() : viewDirection(0.0f, 0.0f, -1.0f), UP(0.0f, 1.0f, 0.0f), distance(3.0)
 {
 }
 
 
 glm::mat4 Camera::getWorldToViewMat() const
 {
-	return glm::lookAt(position, position + viewDirection, UP);
+	return glm::lookAt(cameraPosition, playerPosition, UP);
+}
+
+void Camera::rotate(double phi, double theta)
+{
+	double x = distance * sin(phi);
+	double y = distance * cos(phi) * sin(theta);
+	double z = distance * cos(phi);
+
+	cameraPosition = glm::vec3(x, y, z);
+}
+
+void Camera::updatePlayerPosition(glm::vec3 newPos)
+{
+	playerPosition = newPos;
 }
--- dodgeball_simulator/dodgeball_simulator/camera.h
+++ dodgeball_simulator/dodgeball_simulator/camera.h
@@ -4,13 +4,16 @@
 class Camera
 {
 
-	glm::vec3 position;
+	glm::vec3 cameraPosition;
+	glm::vec3 playerPosition;
 	glm::vec3 viewDirection;
 	const glm::vec3 UP;
+	const double distance;
 
 public:
 	Camera();
 	glm::mat4 getWorldToViewMat() const;
-
+	void rotate(double phi, double theta);
+	void updatePlayerPosition(glm::vec3 newPos);
 };
 
