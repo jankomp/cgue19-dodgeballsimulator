134c481ac18ea8b39069b7d3fe23766de4a66eeb
dorisrh <doris.rhomberg@live.de>
2019-04-06 19:30:01 +0200

    main

--- dodgeball_simulator/dodgeball_simulator/main.cpp
+++ dodgeball_simulator/dodgeball_simulator/main.cpp
@@ -6,9 +6,27 @@
 #include "shader.h"
 //#include "square.h"
 #include "geometry.h"
+#include "model.h"
+#include "guteCamera.h"
 
-//#include "model.h"
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
 
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 int main(void)
 {
